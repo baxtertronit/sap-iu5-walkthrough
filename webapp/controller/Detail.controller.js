65f1903a6227260ceaed6001280d5d77f775c734
branimir.kostic@tron-it.com <branimir.kostic@tron-it.com>
2025-04-07 14:03:40 +0200

    Step 33: Routing Back and History

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/ui/core/UIComponent"],
-  function (Controller, UIComponent) {
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/routing/History",
+  ],
+  function (Controller, UIComponent, History) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
       onInit: function () {
@@ -18,6 +22,16 @@ sap.ui.define(
             ),
           model: "invoice",
         });
+      },
+      onNavBack: function () {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = UIComponent.getRouterFor(this);
+          oRouter.navTo("overview", {}, true /*no history*/);
+        }
       },
     });
   }
